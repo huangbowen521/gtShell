b97ddc60fa9f83d82a5d36413206a12fd9829866
Bowen <huangbowen521@gmail.com>
2013-02-16 16:02:55 +0800

    validate bookmark name.

--- gt.sh
+++ gt.sh
@@ -1,10 +1,11 @@
+#! /bin/bash
+
 DIRS="$HOME/.gtDirs"
 if test ! -e $DIRS 
 then
 	touch $DIRS
 fi
 
- 
 gt () {
 	case $1 in
 		-d)
@@ -14,27 +15,58 @@ gt () {
 			rm -f $temp
 			;;
 		-a)
-			CURDIR=$(echo $PWD)
-			echo "$2=$CURDIR" >> $DIRS
+			isValidName "$@"
+			if [ -z "$result" ]; then
+				CURDIR=$(echo $PWD)
+				echo "$2=$CURDIR" >> $DIRS
+			fi
 			;;
 		-l)
 			cat $DIRS
 			;;
+		-h)
+			print_help
+			;;
 		*)   
-			cd `awk -F '=' '/^'"$1"'=/ {print $2 }' $DIRS`
+			if [ -z $1 ]; then
+				print_help
+			elif [[ ! -z `awk -F '=' '/^'"$1"'=/ {print $2 }' $DIRS` ]]; then
+				cd `awk -F '=' '/^'"$1"'=/ {print $2 }' $DIRS`
+				else
+					echo 'error: bookmark name not found'
+
+			fi
+	
 	esac
 }
 
-function _l {
-awk -F '=' ' {print $1} ' $DIRS
+#validate names
+function isValidName {
+	result="" 
+	if [ -z $2 ]
+		then
+		result='error: name required!'
+		echo $result
+	elif [ "$2" != "$(echo $2 | sed 's/[^A-Za-z0-9_]//g')" ]; then
+		result='error: name is not valid'
+		echo $result
+	fi
+
+}
+
+function print_help {
+	    echo 'Usage:'
+        echo '-a <bookmark_name> - Saves the current directory as "bookmark_name"'
+        echo '-d <bookmark_name> - Deletes the bookmark'
+        echo '-l                 - Lists all available bookmarks'
+        echo '-h(-help,--help)   - List usage'
+        echo '<bookmark_name>    - Jump to the bookmark'   
 }
 
 
 function _comp {
     local curw
     COMPREPLY=()
-    curw=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=($(compgen -W '`_l`' -- $curw))
     return 0
 }
 
@@ -50,7 +82,4 @@ else
 	shopt -s progcomp
 	complete -F _comp gt
 fi
-
-
-
 
